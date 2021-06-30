5a35b54ab2e29bce78131cd2f623e1255d55cda9
tatidan <t.danyliuk@gmail.com>
2021-07-01 00:56:23 +0300

    timer done

--- src/index.js
+++ src/index.js
@@ -1,37 +1,56 @@
-//import './sass/main.scss';
+import './sass/main.scss';
 
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+class CountdownTimer {
+  constructor({ onTick }) {
+    this.onTick = onTick;
+  }
+
+  start() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const targetDate = new Date('Jul 17, 2021');
+      const time = targetDate - currentTime;
+      const timeComponents = this.getTimeComponents(time);
+      this.onTick(timeComponents);
+    }, 1000);
+  }
+
+  pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+  getTimeComponents(time) {
+  const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { days, hours, mins, secs };
+}
+}
+
+const countdownTimer = new CountdownTimer({
+  onTick: updateTimerFace,
 });
+countdownTimer.start();
 
-//Плагин это класс CountdownTimer, экземпляр которого 
-//создает новый таймер с настройками.
 
-//Для подсчета значений используй следующие готовые 
-//формулы, где time - разница между targetDate и текущей датой.
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),        
+}
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+function updateTimerFace({ days, hours, mins, secs }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+// не поняла, как это использовать:
+//  new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate: new Date('Jul 17, 2019'),
+// });
